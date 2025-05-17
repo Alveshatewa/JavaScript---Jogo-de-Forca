75833d9490d80794b43dfb8f19962abc9e27b530
Alves Hatewa <alvesmazy90@gmail.com>
2025-05-17 20:45:21 +0100

    Deploy dos arquivos do projecto

---
+++ dados/dados.js
@@ -0,0 +1,11 @@
+const listaDeFrutas = [
+
+    'Pera', 'Manga', 'Banana', 'Caju', 
+    'Morango', 'Uva', 'Abacaxi', 'Melancia',
+    'Laranja', 'Limao', 'Abacate', 'Coco',
+    'Boiba','Mamao','Pessego','Tangerina',
+
+]
+
+
+export default listaDeFrutas
